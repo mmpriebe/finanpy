d8906e8e9d5c37a2b6030de03b627fa631a42b84
mmpriebe <marciano.priebe@gmail.com>
2026-06-16 18:44:24 -0300

    Ajustado greentings

--- core/context_processors.py
+++ core/context_processors.py
@@ -4,9 +4,11 @@ def finance_globals(request):
     try:
         from accounts.models import Account
         from categories.models import Category
+        from core.views import _build_greeting
         return {
             'global_accounts': Account.objects.filter(user=request.user, is_active=True),
             'global_categories': Category.objects.filter(user=request.user, is_active=True),
+            'greeting': _build_greeting(request.user),
         }
     except Exception:
         return {}
